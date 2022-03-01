6d3c5871ffb2ddb6afc33accbc5e62509a025ce1
Ivanka Todorova <fakeheal@gmail.com>
2022-03-01 14:50:12 +0200

    chore: eject example/ app from expo

--- example/index.js
+++ example/index.js
@@ -1,8 +1,9 @@
-import { registerRootComponent } from 'expo';
+import { AppRegistry, Platform } from 'react-native';
+import App from './App';
 
-import App from './src/App';
+AppRegistry.registerComponent('react-native-intl-phone-field-example', () => App);
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in the Expo client or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+if (Platform.OS === 'web') {
+  const rootTag = document.getElementById('root') || document.getElementById('main');
+  AppRegistry.runApplication('react-native-intl-phone-field-example', { rootTag });
+}
